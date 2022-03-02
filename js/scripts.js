e7d8b5701bb00935f70cb5d7d71619a39e422c3b
valehmoghaddam <valeh.moghaddam@deakin.edu.au>
2022-03-02 18:30:37 +1100

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-function changeText(){}
-alert("Hello SIT725")
+function changeText(){
+  //  debugger;
+//alert("Hello SIT725")
 console.log ("hello everyone!")
+document.getElementById("heading").style="color:red" 
+document.getElementById("heading").innerText="Hi everybody I'm working" 
 }
